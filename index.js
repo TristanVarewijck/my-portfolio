12a323f3d03660b3ce32682cfa145add375b7099
TristanVarewijck <tristanconnect4@outlook.com>
2021-02-28 22:59:39 +0100

    server + pugfiles

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.use(express.static("static"));
 // pug setup
 app.set("view engine", "pug");
 
+// render 
+app.get('/', function (req, res) {
+res.render('index', { title: 'WORK' })
+ })
+
 // express localhost port
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
--- static/scripts/app.js
+++ static/scripts/app.js
@@ -12,3 +12,27 @@ window.onscroll = function () {
 }
 
 
+// changing words 
+
+const jobs = ["Front-end developer", "UI/UX Designer", "Student"];
+const wordChanger = document.getElementById("change-words");
+
+function jobDisplayer() {
+  let i = 0;
+  return function(jobs) {
+    let interval = setInterval(() => {
+      if (i < jobs.length) {
+        wordChanger.textContent = jobs[i];
+        i++;
+      } else {
+        clearInterval(interval);
+        i = 0;
+     }
+    }, 1000);
+  }
+}
+
+let displayjobs = jobDisplayer();
+displayjobs(jobs);
+
+
